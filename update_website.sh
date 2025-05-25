9e60bc15817faedd6e5c2d31d55cbc7c4e4d4cdb
Palegg Works <palegg-works@gmail.com>
2025-05-25 19:50:44 -0400

    update website

--- update_website.sh
+++ update_website.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 set -e
 
+rm -rf ./docs
 dx bundle --platform web --release
-cp -r ./target/dx/palegg-works-github-io/release/web/public ./docs
+cp -rr ./target/dx/palegg-works-github-io/release/web/public ./docs
 
 echo Website source code updated!
 
