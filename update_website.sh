85b7b6f66892043ce11928f169a03f33bc0a29e9
Palegg Works <palegg-works@gmail.com>
2025-08-22 19:16:49 -0400

    add analytics

--- update_website.sh
+++ update_website.sh
@@ -5,5 +5,22 @@ rm -rf ./docs
 dx bundle --platform web --release
 cp -rr ./target/dx/palegg-works-github-io/release/web/public ./docs
 
+cd docs
+
+# Add Simple Analytics
+file="index.html"
+snippet='<!-- 100% privacy-first analytics --><script data-collect-dnt="true" async src="https://scripts.simpleanalyticscdn.com/latest.js"></script><noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif?collect-dnt=true" alt="" referrerpolicy="no-referrer-when-downgrade"/></noscript>'
+if sed --version >/dev/null 2>&1; then
+  # GNU sed
+  sed -i "/<\/body>/i $snippet" "$file"
+else
+  # BSD/macOS sed
+  sed -i '' "/<\/body>/i\\
+$snippet
+" "$file"
+fi
+
+cd ..
+
 echo Website source code updated!
 
